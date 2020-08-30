4b6bbee4771e71a582b1b5d247ec6c02f797d04d
Paul Barker <paul.barker@sancloud.co.uk>
2020-08-30 11:22:26 +0100

    CI: Reduce artifacts size & eliminate symlinks
    Signed-off-by: Paul Barker <paul.barker@sancloud.co.uk>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -4,6 +4,21 @@ set -e
 
 CONF=$1
 
+mkdir images
+IMAGES_DIR=`realpath images`
+
 kas build --update --force-checkout kas/dev/${CONF}.yml:kas/inc/ci.yml
 
-mv build/tmp/deploy/images .
+cd build/tmp/deploy/images/bbe
+cp  MLO \
+    am335x-sancloud-bbe.dtb \
+    am335x-sancloud-bbe-icu4.dtb \
+    am335x-sancloud-bbei-wifi.dtb  \
+    core-image-base-bbe.wic.bmap \
+    core-image-base-bbe.wic.xz \
+    core-image-base.env \
+    modules-bbe.tgz \
+    u-boot.img \
+    u-boot-initial-env \
+    zImage \
+    ${IMAGES_DIR}
